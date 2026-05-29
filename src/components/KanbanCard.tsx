3c1d3ea8a60a0567bc4d65b1a99a548264a4a718
ZephyroYz <luisangel17050713@gmail.com>
2026-05-29 17:14:42 -0600

    fix: cards text size

--- src/components/KanbanCard.tsx
+++ src/components/KanbanCard.tsx
@@ -9,10 +9,12 @@ interface KanbanCardProps{
 
 export default function KanbanCards({id, title, desc}: KanbanCardProps){
     return(
-        <div className="border border-gray-300 p-4 rounded-md bg-gray-50 mt-2">
-            <h4 className="text-md"> {title} </h4>
+        <div className="border border-gray-400 p-4 rounded-md bg-gray-50 mt-2">
+            <h4 className="text-base mb-2"> {title} </h4>
             {desc && (
-                <p className="text-sm whitespace-pre-wrap"> {desc} </p>
+                <p className="text-sm whitespace-pre-wrap">
+                    {desc} 
+                </p>
             )}
             
             <span className="text-slate-400 text-xs">
